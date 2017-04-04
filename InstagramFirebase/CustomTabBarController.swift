e14f7517ed5bb51cf73ac58213bbf0b61479a8a0
Joe Kim <josephkim0224@gmail.com>
2017-04-04 15:54:38 -0400

    Add four more tab bar icons and fixed the alignment of tab bar icons

--- InstagramFirebase/CustomTabBarController.swift
+++ InstagramFirebase/CustomTabBarController.swift
@@ -30,22 +30,31 @@ class CustomTabBarController: UITabBarController {
         let userProfileVC = UserProfileController(collectionViewLayout: layout)
         userProfileVC.navigationItem.title = "Temp"
         let userProfileViewController = UINavigationController(rootViewController: userProfileVC)
-        userProfileViewController.title = "Profile"
         userProfileViewController.tabBarItem.image = #imageLiteral(resourceName: "profile_unselected")
         userProfileViewController.tabBarItem.selectedImage = #imageLiteral(resourceName: "profile_selected")
         
         tabBar.tintColor = .black
         
-        viewControllers = [userProfileViewController, createTemporaryController(title: "Temp", imageName: "temp"), createTemporaryController(title: "Temp", imageName: "temp"), createTemporaryController(title: "Temp", imageName: "temp"), createTemporaryController(title: "Temp", imageName: "temp")]
+        viewControllers = [createTemporaryController(title: "Home", image: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: UserProfileController(collectionViewLayout: UICollectionViewFlowLayout())),
+                           createTemporaryController(title: "Search", image: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected")),
+                           createTemporaryController(title: "Add", image: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected")),
+                           createTemporaryController(title: "Like", image: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected")),
+                           userProfileViewController]
+        
+        guard let items = tabBar.items else { return }
+        
+        for item in items {
+            item.imageInsets = UIEdgeInsets(top: 4, left: 0, bottom: -4, right: 0)
+        }
     }
     
-    fileprivate func createTemporaryController(title: String, imageName: String) -> UINavigationController {
-        let viewController = UIViewController()
+    fileprivate func createTemporaryController(title: String, image: UIImage, selectedImage: UIImage, rootViewController: UIViewController = UIViewController()) -> UINavigationController {
+        let viewController = rootViewController
         viewController.navigationItem.title = title
         viewController.view.backgroundColor = .white
         let navController = UINavigationController(rootViewController: viewController)
-        navController.title = title
-        navController.tabBarItem.image = UIImage(named: imageName)
+        navController.tabBarItem.image = image
+        navController.tabBarItem.selectedImage = selectedImage
         return navController
     }
 }
