542242338a4f91cc1211b0bbc666a9a6b614584f
Joe Kim <josephkim0224@gmail.com>
2017-04-05 09:00:51 -0400

    Finished first page of photo selection

--- InstagramFirebase/PhotoSelectorController.swift
+++ InstagramFirebase/PhotoSelectorController.swift
@@ -13,8 +13,13 @@ class PhotoSelectorController: UICollectionViewController, UICollectionViewDeleg
     
     fileprivate let cellId = "cellId"
     fileprivate let headerId = "headerId"
-    
+    fileprivate let photoFetchLimit = 10
+    fileprivate let photoTargetSizeLengthSmall: CGFloat = 200
+    fileprivate let photoTargetSizeLengthLarge: CGFloat = 720
+
+    var assets = [PHAsset]()
     var images = [UIImage]()
+    var selectedImage: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +29,7 @@ class PhotoSelectorController: UICollectionViewController, UICollectionViewDeleg
         setupNavigationButtons()
         
         collectionView?.register(PhotoSelectorCell.self, forCellWithReuseIdentifier: cellId)
-        collectionView?.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
+        collectionView?.register(PhotoSelectorHeader.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerId)
         
         fetchPhotos()
     }
@@ -35,29 +40,46 @@ class PhotoSelectorController: UICollectionViewController, UICollectionViewDeleg
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Next", style: .plain, target: self, action: #selector(handleNext))
     }
     
-    fileprivate func fetchPhotos() {
+    fileprivate func assetsFetchOptions() -> PHFetchOptions {
         let fetchOptions = PHFetchOptions()
-        fetchOptions.fetchLimit = 10
+        fetchOptions.fetchLimit = photoFetchLimit
         let sortDescriptor = NSSortDescriptor(key: "creationDate", ascending: false)
         fetchOptions.sortDescriptors = [sortDescriptor]
-        let allPhotos = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+        return fetchOptions
+    }
+    
+    fileprivate func fetchPhotos() {
         
-        allPhotos.enumerateObjects({ (asset, count, stop) in
-            let imageManager = PHImageManager.default()
-            let targetSize = CGSize(width: 350, height: 350)
-            let options = PHImageRequestOptions()
-            options.isSynchronous = true
+        let allPhotos = PHAsset.fetchAssets(with: .image, options: assetsFetchOptions())
+        
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            guard let this = self else { return }
+
+            allPhotos.enumerateObjects({ (asset, count, stop) in
+                let imageManager = PHImageManager.default()
+                let targetSize = CGSize(width: this.photoTargetSizeLengthSmall, height: this.photoTargetSizeLengthSmall)
+                let options = PHImageRequestOptions()
+                options.isSynchronous = true
             
-            imageManager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFit, options: options, resultHandler: { [weak self] (image, info) in
-                if let image = image {
-                    self?.images.append(image)
-                }
-                
-                if count == allPhotos.count - 1 {
-                    self?.collectionView?.reloadData()
-                }
+                imageManager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFit, options: options, resultHandler: { (image, info) in
+                    if let image = image {
+                        this.images.append(image)
+                        this.assets.append(asset)
+                        
+                        if this.selectedImage == nil {
+                            this.selectedImage = image
+                        }
+                    }
+                    
+                    if count == allPhotos.count - 1 {
+                        DispatchQueue.main.async {
+                            this.collectionView?.reloadData()
+                        }
+                    }
+                })
+                print(this.images)
             })
-        })
+        }
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -70,6 +92,11 @@ class PhotoSelectorController: UICollectionViewController, UICollectionViewDeleg
         cell.photoImageView.image = images[indexPath.item]
         
         return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        selectedImage = images[indexPath.item]
+        collectionView.reloadData()
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -86,8 +113,20 @@ class PhotoSelectorController: UICollectionViewController, UICollectionViewDeleg
     }
     
     override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath)
-        header.backgroundColor = .red
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerId, for: indexPath) as! PhotoSelectorHeader
+
+        header.photoImageView.image = selectedImage
+
+        guard let selectedImage = selectedImage, let index = images.index(of: selectedImage) else { return header }
+        
+        let selectedAsset = assets[index]
+        let targetSize = CGSize(width: photoTargetSizeLengthLarge, height: photoTargetSizeLengthLarge)
+        
+        let imageManager = PHImageManager.default()
+        imageManager.requestImage(for: selectedAsset, targetSize: targetSize, contentMode: .default, options: nil) { (image, info) in
+            header.photoImageView.image = image
+        }
+        
         return header
     }
     
